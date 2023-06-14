f099b585512e41414aecf64557f061f69b7ea920
ippeiman <hawaippei6@gmail.com>
2023-06-14 15:29:16 +0900

    no.8 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ippei' && password == '2928'
+    end
+  end
 end
