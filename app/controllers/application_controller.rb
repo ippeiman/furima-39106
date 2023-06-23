66b0a1ad228349d076d8640339b21d17016bac9a
ippeiman <hawaippei6@gmail.com>
2023-06-23 19:29:40 +0900

    新規登録/ユーザー情報

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'ippei' && password == '2928'
     end
+    #protospceのもの
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+    end
   end
 end
+  
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :order_informations
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_katakana, presence: true
+  validates :last_name_katakana, presence: true
+  validates :birth_date, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A.+@.+..+\z/, message: 'は有効でありません。' }
+  validates :password, length: { minimum: 6 }
+  validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください。' }
+  validates :password_confirmation, presence: true, on: :create
 end
