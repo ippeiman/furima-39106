2b4938defdfd09b38a24a720793a2603754d8089
ippeiman <hawaippei6@gmail.com>
2023-06-23 20:27:12 +0900

    新規登録/本人情報確認

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'ippei' && password == '2928'
     end
-    #protospceのもの
+    
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name_katakana, presence: true
-  validates :last_name_katakana, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
   validates :birth_date, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: /\A.+@.+..+\z/, message: 'は有効でありません。' }
