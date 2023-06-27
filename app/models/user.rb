38c7a43a60b5ea7a1570ea2a190b3273e6705fc7
ippeiman <hawaippei6@gmail.com>
2023-06-27 20:21:47 +0900

    新規登録成功！

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :order_informations
+        
+  #has_many :items
+  #has_many :order_informations
 
   validates :nickname, presence: true
   validates :first_name, presence: true
@@ -13,9 +14,4 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
   validates :birth_date, presence: true
-  validates :email, uniqueness: true
-  validates :email, format: { with: /\A.+@.+..+\z/, message: 'は有効でありません。' }
-  validates :password, length: { minimum: 6 }
-  validates :password, format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i, message: 'は英字と数字の両方を含めて設定してください。' }
-  validates :password_confirmation, presence: true, on: :create
 end
