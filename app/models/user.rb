94d56bb80c60416199ca50e603fd0c3022f58539
ippeiman <hawaippei6@gmail.com>
2023-07-02 19:55:16 +0900

    ユーザー管理機能rev.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,12 @@ class User < ApplicationRecord
   #has_many :items
   #has_many :order_informations
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birth_date, presence: true
+validates :nickname, presence: true
+validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "can only contain letters" }
+validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "can only contain letters" }
+validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "can only contain katakana" }
+validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "can only contain katakana" }
+validates :birth_date, presence: true
+
 
 end
