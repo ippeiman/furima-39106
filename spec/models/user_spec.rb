f018ae964bf94f7aa68e20ccbf5e4a3f1c290d3e
ippeiman <hawaippei6@gmail.com>
2023-07-04 14:46:35 +0900

    ユーザー管理機能rev.7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,16 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
+      it "first_name_kanaが空では登録できない" do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it "last_name_kanaが空では登録できない" do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
       it "birth_dateが空では登録できない" do
         @user.birth_date = ''
         @user.valid?
